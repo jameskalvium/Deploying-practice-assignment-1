c3bf52cf50a76c6541f7e45bee36cb93556446ce
unknown <james.r@kalvium.community>
2024-12-12 11:30:21 +0530

    made a server file

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const config = {
+    apiKey: process.env.API_KEY,
+    serverSecret: process.env.SERVER_SECRET,
+    isKalvian: process.env.IS_KALVIAN === 'true',
+  };
+
+
+  console.log(config);
+
+  
+  module.exports = config;
